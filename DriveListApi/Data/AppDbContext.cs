7c5c07504cf84484a8482eceb1291cbf3ffd56de
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-13 13:06:47 +0300

    Proje dosyası ekle.

---
+++ DriveListApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DriveListApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriveListApi.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Prediction> Predictions { get; set; }
+    }
+}
---
+++ DriveListApi/Models/Prediction.cs
@@ -0,0 +1,16 @@
+﻿namespace DriveListApi.Models
+{
+    public class Prediction
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+        public string GearType { get; set; }
+        public string FuelType { get; set; }
+        public string City { get; set; }
+        public double PredictedPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
