cd197997a49f12cc9f72f70b3587e80d8adcd4e9
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-17 22:33:40 +0300

    Proje dosyası ekle.

--- DriveListApi/Data/AppDbContext.cs
+++ DriveListApi/Data/AppDbContext.cs
@@ -8,5 +8,6 @@ namespace DriveListApi.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Prediction> Predictions { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
