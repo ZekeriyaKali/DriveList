3c0b60824b78737eeac11f825d7ae6863a2f36b3
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-20 20:44:04 +0300

    Proje dosyası ekle.

--- DriveListApi/Controllers/CarPredictionController.cs
+++ DriveListApi/Controllers/CarPredictionController.cs
@@ -32,16 +32,21 @@ namespace DriveListApi.Controllers
 
             var prediction = await response.Content.ReadFromJsonAsync<PredictionResponse>();
 
-            ViewBag.Brand = request.Brand;
-            ViewBag.Model = request.Model;
-            ViewBag.Year = request.Year;
-            ViewBag.Km = request.Km;
-            ViewBag.GearType = request.GearType;
-            ViewBag.FuelType = request.FuelType;
-            ViewBag.City = request.City;
-            ViewBag.PredictedPrice = prediction.PricePrediction;
 
-            return View("Result", prediction);
+            var vm = new PredictionViewModel
+            {
+                Brand = request.Brand,
+                Model = request.Model,
+                Year = request.Year,
+                Km = request.Km,
+                GearType = request.GearType,
+                FuelType = request.FuelType,
+                City = request.City,
+                PredictedPrice = prediction.PricePrediction
+            };
+
+
+            return View("Result", vm);
         }
     }
 }
---
+++ DriveListApi/Models/PredictionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DriveListApi.Models
+{
+    public class PredictionViewModel
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+        public string GearType { get; set; }
+        public string FuelType { get; set; }
+        public string City { get; set; }
+        public double PredictedPrice { get; set; }
+    }
+}
