2880d6d6b8d255b4c040d8be9916eb33fd41b091
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-21 12:15:06 +0300

    Proje dosyası ekle

---
+++ DriveListApi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DriveListApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
