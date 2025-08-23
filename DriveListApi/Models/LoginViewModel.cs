89af38ee10bb7b42b01a5db53cbfa76d64c55551
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-23 22:04:45 +0300

    Proje dosyası ekle.

---
+++ DriveListApi/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriveListApi.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
