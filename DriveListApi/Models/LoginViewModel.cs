a715a34b1a63a986c8460924af340e0adf0e5254
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-24 17:13:43 +0300

    Proje dosyası ekle.

--- DriveListApi/Models/LoginViewModel.cs
+++ DriveListApi/Models/LoginViewModel.cs
@@ -4,12 +4,16 @@ namespace DriveListApi.Models
 {
     public class LoginViewModel
     {
-        [Required]
-        public string Username { get; set; }
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string Username { get; set; } = "";
 
-        [Required, DataType(DataType.Password)]
-        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre zorunludur.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; } = "";
 
+        [Display(Name = "Beni Hatırla")]
         public bool RememberMe { get; set; }
     }
 }
--- DriveListApi/Models/RegisterViewModel.cs
+++ DriveListApi/Models/RegisterViewModel.cs
@@ -4,16 +4,25 @@ namespace DriveListApi.Models
 {
     public class RegisterViewModel
     {
-        [Required]
-        public string Username { get; set; }
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string Username { get; set; } = "";
 
-        [Required, EmailAddress]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "E-posta zorunludur.")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta giriniz.")]
+        [Display(Name = "E-Posta")]
+        public string Email { get; set; } = "";
 
-        [Required, DataType(DataType.Password)]
-        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre zorunludur.")]
+        [StringLength(100, ErrorMessage = "Şifre en az {2} karakter olmalı.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; } = "";
 
-        [Required, DataType(DataType.Password), Compare("Password")]
-        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Şifre tekrar zorunludur.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor.")]
+        [Display(Name = "Şifre (Tekrar)")]
+        public string ConfirmPassword { get; set; } = "";
     }
 }
