9d5166eafed7530d911cf841e052cc9990274f79
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-08-28 21:13:15 +0300

    Parola sıfırlama model ekleme

---
+++ DriveListApi/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriveListApi.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Yeni şifre giriniz.")]
+        [StringLength(100, ErrorMessage = "{0} en az {2} karakter olmalıdır.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifreyi Onayla")]
+        [Compare("Password", ErrorMessage = "Şifreler eşleşmiyor.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
