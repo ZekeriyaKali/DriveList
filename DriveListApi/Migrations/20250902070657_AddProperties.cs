f2c5b5b2d185e8d18b8e9ce2f6b14b320976095b
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-02 10:08:48 +0300

    feat(auth): add LastLoginTime  and UserId column to ApplicationUser and Prediction via migration

---
+++ DriveListApi/Migrations/20250902070657_AddProperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DriveListApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Predictions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginTime",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Predictions");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginTime",
+                table: "AspNetUsers");
+        }
+    }
+}
