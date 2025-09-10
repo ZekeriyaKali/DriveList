46af5d24024e2c7d6e6a305ebfefbbac4d08e219
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-10 20:09:26 +0300

    feat(user): add Credits property to ApplicationUser

--- DriveListApi/Models/ApplicationUser.cs
+++ DriveListApi/Models/ApplicationUser.cs
@@ -5,5 +5,6 @@ namespace DriveListApi.Models
     public class ApplicationUser : IdentityUser
     {
         public DateTime? LastLoginTime { get; set; } // Kullanıcının son giriş zamanı
+        public int Credits { get; set; } = 10; // Başlangıç kredisi
     }
 }
