8633a733158dc73a080829e214a33ee9849c0545
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-10 20:11:03 +0300

    chore(db): create migration for Credits column

---
+++ DriveListApi/Migrations/20250910171032_AddCreditsToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DriveListApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreditsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Credits",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Credits",
+                table: "AspNetUsers");
+        }
+    }
+}
