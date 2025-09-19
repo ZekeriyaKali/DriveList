71e5585b44d648b1bcf85434d0952dc8e2050916
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-19 22:33:07 +0300

    feat(shared): add CarRequest model to shared library

---
+++ DriveList.Shared/Models/CarRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Shared.Models
+{
+    public class CarRequest
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+        public string GearType { get; set; }
+        public string FuelType { get; set; }
+        public string City { get; set; }
+    }
+}
