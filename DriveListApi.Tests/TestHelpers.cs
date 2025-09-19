589b50e7f648e9c8595c254e0eb8b81d94084809
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-19 22:07:35 +0300

    test(helpers): add TestHelpers utility for mocking UserManager and SignInManager

---
+++ DriveListApi.Tests/TestHelpers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace DriveListApi.Tests
+{
+    public static class TestHelpers
+    {
+        // Sahte UserManager üretir
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            return new Mock<UserManager<TUser>>(
+                store.Object,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+            );
+        }
+
+        // Sahte SignInManager üretir
+        public static Mock<SignInManager<TUser>> MockSignInManager<TUser>(
+            Mock<UserManager<TUser>> userManager) where TUser : class
+        {
+            return new Mock<SignInManager<TUser>>(
+                userManager.Object,
+                new Mock<IHttpContextAccessor>().Object,
+                new Mock<IUserClaimsPrincipalFactory<TUser>>().Object,
+                null, null, null, null
+            );
+        }
+    }
+}
