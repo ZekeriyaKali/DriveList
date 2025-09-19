317aba963f5dca65a320249320fe568c87682f33
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-19 22:34:43 +0300

    feat(shared): add PredictionResponse model to shared library

---
+++ DriveList.Shared/Models/PredictionResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Shared.Models
+{
+    public class PredictionResponse
+    {
+        public double PricePrediction { get; set; }
+    }
+}
