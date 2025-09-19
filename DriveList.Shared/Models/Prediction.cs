620b6bdd6bc73a67477efa1a6101aab75b8f7d9b
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-19 22:33:55 +0300

    feat(shared): add Prediction model to shared library

---
+++ DriveList.Shared/Models/Prediction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Shared.Models
+{
+    public class Prediction
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+        public string GearType { get; set; }
+        public string FuelType { get; set; }
+        public string City { get; set; }
+        public double PredictedPrice { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string UserId { get; set; }
+    }
+}
