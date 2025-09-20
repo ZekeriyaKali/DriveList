63a142b61f3c5ccf79cecf098d20676831502f10
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-20 19:19:25 +0300

    feat(application): add CarPrediction service interface

---
+++ DriveList.Application/Services/ICarPredictionService.cs
@@ -0,0 +1,14 @@
+﻿using DriveListApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Application.Services
+{
+    public interface ICarPredictionService
+    {
+        Task<PredictionResponse> PredictAsync(CarRequest request);
+    }
+}
