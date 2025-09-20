658b46aaec774d57227a5b3663f5785ad3eb01c0
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-20 19:32:08 +0300

    feat(application): implement CarPredictionService with Flask API integration and DB save

---
+++ DriveList.Application/Services/CarPredictionService.cs
@@ -0,0 +1,55 @@
+﻿using DriveListApi.Data;
+using DriveListApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Application.Services
+{
+    public class CarPredictionService : ICarPredictionService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly AppDbContext _context;
+
+        public CarPredictionService(IHttpClientFactory httpClientFactory, AppDbContext context)
+        {
+            _httpClientFactory = httpClientFactory;
+            _context = context;
+        }
+
+        public async Task<PredictionResponse> PredictAsync(CarRequest request)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var flaskResponse = await client.PostAsJsonAsync("http://localhost:5000/predict", request);
+
+            if (flaskResponse.IsSuccessStatusCode)
+            {
+                throw new Exception("Tahmin API hatası");
+            }
+
+            var result = await flaskResponse.Content.ReadFromJsonAsync<PredictionResponse>();
+
+            var prediction = new Prediction
+            {
+                Brand = request.Brand,
+                Model = request.Model,
+                Year = request.Year,
+                Km = request.Km,
+                GearType = request.GearType,
+                FuelType = request.FuelType,
+                City = request.City,
+                PredictedPrice = result.PricePrediction,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.Predictions.Add(prediction);
+            await _context.SaveChangesAsync();
+
+            return result!;
+
+        }
+    }
+}
