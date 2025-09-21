282055a7cef54b45c812246934dc787ded4f7997
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-21 20:32:21 +0300

    refactor(infrastructure): move ApplicationUser model into Infrastructure layer

---
+++ DriveList.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DriveListApi.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime? LastLoginTime { get; set; } // Kullanıcının son giriş zamanı
+        public int Credits { get; set; } = 10; // Başlangıç kredisi
+    }
+}
