2fb2631203d9f0990235c83751a0b7f5e7ff5c2d
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-23 00:08:04 +0300

    feat(infrastructure): add PredictionRepository with IPredictionRepository interface for managing predictions

---
+++ DriveList.Application/Common/Interfaces/IPredictionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Application.Common.Interfaces
+{
+    public interface IPredictionRepository
+    {
+        Task AddAsync(Prediction prediction);
+        Task<IEnumerable<Prediction>> GetAllAsync();
+        Task<Prediction?> GetByIdAsync(int id);
+    }
+}
---
+++ DriveList.Infrastructure/Persistence/Repositories/PredictionRepository.cs
@@ -0,0 +1,36 @@
+﻿using DriveList.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveList.Infrastructure.Persistence.Repositories
+{
+    public class PredictionRepository: IPredictionRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PredictionRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Prediction prediction)
+        {
+            await _context.Predictions.AddAsync(prediction);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Prediction>> GetAllAsync()
+        {
+            return await _context.Predictions.ToListAsync();
+        }
+
+        public async Task<Prediction> GetByIdAsync(int id)
+        {
+            return await _context.Predictions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
