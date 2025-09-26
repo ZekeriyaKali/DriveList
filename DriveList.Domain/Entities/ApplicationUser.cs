a4c83361eb85a62da34bb679579df4c3f4e267ab
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-26 22:45:34 +0300

    feat(domain): add nuget package for using identity

---
+++ DriveList.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+namespace DriveList.Domain.Entities { 
+
+public class ApplicationUser : IdentityUser
+{
+    public DateTime? LastLoginTime { get; set; } // Kullanıcının son giriş zamanı
+    public int Credits { get; set; } = 10; // Başlangıç kredisi
+}
+}
