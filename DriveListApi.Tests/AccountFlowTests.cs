473b035c62ef60432932c96617cb2075ce11b32d
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-28 22:45:22 +0300

    test(integration): add AccountController registration flow test

---
+++ DriveListApi.Tests/AccountFlowTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveListApi.Tests
+{
+    public class AccountFlowTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public AccountFlowTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Register_Should_CreateUser()
+        {
+            var response = await _client.PostAsJsonAsync("/account/register", new
+            {
+                Username = "testuser",
+                Email = "test@test.com",
+                Password = "Test1234!"
+            });
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
