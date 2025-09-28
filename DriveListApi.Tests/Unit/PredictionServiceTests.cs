fb4875f795627d96742465e2ea140287a4803700
ZekeriyaKali <108419937+ZekeriyaKali@users.noreply.github.com>
2025-09-28 22:43:18 +0300

    refactor(test): PredictionServiceTest move to unit directory

--- DriveListApi.Tests/Unit/PredictionServiceTests.cs
+++ DriveListApi.Tests/Unit/PredictionServiceTests.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DriveListApi.Tests
+namespace DriveListApi.Tests.Unit
 {
     public class PredictionServiceTests
     {
